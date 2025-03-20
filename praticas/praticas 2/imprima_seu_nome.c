fe8a398f0d6840cd42fc94c3fc0d1446219fbda3
tigaspje <thiagomatilha@gmail.com>
2025-03-19 21:58:54 -0300

    Adicionando arquivos da pratica02

---
+++ praticas/praticas 2/imprima_seu_nome.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("\nthiago goncalves de medeiros\n");
+
+  return 0;
+}
---
+++ praticas/praticas 2/imprimir_boletim.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("%i\t%f/n"),
+    printf(" Matricula	\tA1\tA2\tMedia\n");
+    printf("%i\t%f/n",20210010,	8.5	,5.0	, 6.4 );
+    printf("%i\t%f/n",20215053,	4.7	,10.0, 7.9);
+    printf("%i\t%f/n",20220027,	5.6,	8.3	 ,7.2);
+    printf("%i\t%f/n",20220103,	9.2	,1.5	, 4.6);
+
+  return 0;
+}
