82efe62e115ef90f396070c73dd2458958d8136d
tigaspje <thiagomatilha@gmail.com>
2025-03-26 21:59:53 -0300

    Adicionando arquivos da pratica03

---
+++ praticas/pratica 03/pratica03.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+int main() {
+    printf("O tipo 'char' aceita valores entre %i e %i.\n", CHAR_MIN, CHAR_MAX);
+
+  return 0;
+}
---
+++ praticas/pratica 03/tamanho_tipo.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main() {
+    printf("O tipo 'char' ocupa %lu byte(s) na memoria.\n", sizeof(char));
+    printf("O tipo 'int' ocupa %lu byte(s) na memoria.\n", sizeof(int));
+    printf("O tipo 'float' ocupa %lu byte(s) na memoria.\n", sizeof(float));
+    printf("O tipo 'double' ocupa %lu byte(s) na memoria.\n", sizeof(double));
+    printf("O tipo 'void' ocupa %lu byte(s) na memoria.\n", sizeof(void));
+
+  return 0;
+}
+
